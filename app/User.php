b65d3201ee24afac236a65fcf6e31c70f64621cf
Gesoaldo <gesoaldo.guimaraes@academico.ifpb.edu.br>
2018-07-28 17:53:52 -0300

    models and news migrations

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'login', 'email', 'password', 'photo', 'isAdmin'
     ];
 
     /**
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    public function userable(){ //se tornar um user
+        return $this->morphTo();
+    }
 }
