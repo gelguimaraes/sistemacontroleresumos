c6e7273dc6a506b9fcd8f4ea4d78988b796fc544
Gesoaldo <gesoaldo.guimaraes@academico.ifpb.edu.br>
2018-07-30 10:13:31 -0300

    models and news migrations

--- app/Http/Controllers/ControllerAdmin.php
+++ app/Http/Controllers/ControllerAdmin.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class ControllerAdmin extends Controller
 {
-    //
+
+
+    public function createForm(){
+        return view("admin");
+    }
+
+    public function get(Request $req, int $id){
+
+    }
+
+    public function add(string $nome){
+
+    }
+
+    public function delete(int $id){
+
+    }
+
+    public function update(int $id){
+
+    }
+
+    public function list(){
+
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -17,69 +17,21 @@ Route::get('/', function () {
 
 //usuarios
 //Route::get('/', 'ControllerUser@index');
-Route::post('addResumo', 'ControllerUser@add');
-Route::post('viewTrabalhos', 'ControllerUser@view');
+Route::post('resumo', 'ControllerResumo@createForm');
 
-Route:: group(['prefix' => 'admin', 'middleware'=>'auth'], function(){
-//Faculdade
-    Route::post('addFaculdade', 'ControllerFaculdade@add');
-    Route::delete('deleteFaculdade/{id}', 'ControllerFaculdade@delete');
-    Route::get('getFaculdade/{id}', 'ControllerFaculdade@get');
-    Route::get('listFaculdade', 'ControllerFaculdade@list');
-    Route::get('formFaculdade', 'ControllerFaculdade@createForm');
-    Route::put('updadeFaculdade/{id}', 'ControllerFaculdade@update');
-    Route::get('editFormFaculdade/{id}', 'ControllerFaculdade@edit');
 
-//Semestre
-    Route::post('addSemestre', 'ControllerSemestre@add');
-    Route::delete('deleteSemestre/{id}', 'ControllerSemestre@delete');
-    Route::get('getSemestre/{id}', 'ControllerSemestre@get');
-    Route:: group(['prefix' => 'listSemestre'], function(){
-        Route::get('/', 'ControllerSemestre@list'); ///criar grupo para o mesmo controler setar valor null para os parametros
-        Route::get('/faculdade/{id?}', 'ControllerSemestre@list');
-    });
-    Route::get('formSemestre', 'ControllerSemestre@createForm');
-    Route::put('updadeSemestre/{id}', 'ControllerSemestre@update');
-    Route::get('editFormSemestre/{id}', 'ControllerSemestre@edit');
+Route:: group(['prefix' => 'admin'], function(){
 
-//Disciplina
-    Route::post('addDisciplina', 'ControllerDisciplina@add');
-    Route::delete('deleteDisciplina/{id}', 'ControllerDisciplina@delete');
-    Route::get('getDisciplina/{id}', 'ControllerDisciplina@get');
-    Route::get('listDisciplina/faculdade/{id?}/{semestre?}/{id?}', 'ControllerDisciplina@list');
-    Route::get('formDisciplina', 'ControllerDisciplina@createForm');
-    Route::put('updadeDisciplina/{id}', 'ControllerDisciplina@update');
-    Route::get('editFormDisciplina/{id}', 'ControllerDisciplina@edit');
-
-//Tema
-    Route::post('addTema', 'ControllerTema@add');
-    Route::delete('deleteTema/{id}', 'ControllerTema@delete');
-    Route::get('getTema/{id}', 'ControllerTema@get');
-    Route::get('listTema', 'ControllerTema@list');
-    Route::get('formTema', 'ControllerTema@createForm');
-    Route::put('updadeTema/{id}', 'ControllerTema@update');
-    Route::get('editFormTema/{id}', 'ControllerTema@edit');
-
-//Login
-    Route::post('addLogin', 'ControllerLogin@add');
-    Route::delete('deleteLogin/{id}', 'ControllerLogin@delete');
-    Route::get('getLogin/{id}', 'ControllerLogin@get');
-    Route::get('listLogin', 'ControllerLogin@list');
-    Route::get('formLogin', 'ControllerLogin@createForm');
-    Route::put('updadeLogin/{id}', 'ControllerLogin@update');
-    Route::get('editFormLogin/{id}', 'ControllerLogin@edit');
-
-//Resumo
-    Route::post('addResumo', 'ControllerResummo@add');
-    Route::delete('deleteResumo/{id}', 'ControllerResumo@delete');
-    Route::get('getResumo/{id}', 'ControllerResumo@get');
-    Route::get('listResumo', 'ControllerResumo@list');
-    Route::get('formResumo', 'ControllerResumo@createForm');
-    Route::put('updadeResumo/{id}', 'ControllerResumo@update');
-    Route::get('editFormResumo/{id}', 'ControllerResumo@edit');
+//    Route::post('addFaculdade', 'ControllerResumo@add');
+//    Route::delete('deleteFaculdade/{id}', 'ControllerResumo@delete');
+//    Route::get('getFaculdade/{id}', 'ControllerResumo@get');
+//    Route::get('listFaculdade', 'ControllerResumo@list');
+//    Route::get('formFaculdade', 'ControllerResumo@createForm');
+//    Route::put('updadeFaculdade/{id}', 'ControllerResumo@update');
+//    Route::get('editFormFaculdade/{id}', 'ControllerResumo@edit');
 
  //Index
-    Route:: get('/', 'ControllerAdmin@index');
+    Route:: get('/', 'ControllerAdmin@createForm');
 });
 
 
